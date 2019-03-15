b1ca783fff9446d7549f99a474d0d6a90ca17b42
amareharuno <leremeeva174@gmail.com>
2019-03-15 18:10:39 +0300

    Task 5

--- pz1/Task5.js
+++ pz1/Task5.js
@@ -2,3 +2,13 @@
     5. Условие: дан объект {js: "forever", java: "never"}.
     Задание: вывести в консоль объект в формате: свойсво - значение. Использывать цикл for in.
 */
+console.log("Task 5");
+const object = {js: "forever", java: "never"};
+console.log("Initial Object:");
+console.log(object);
+
+
+console.log("Result:");
+for (let prop in object) {
+    console.log(`${prop} - ${object[prop]}`);
+}
