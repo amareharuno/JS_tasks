8b10e1a45e1f8afbc46199841fecffb1f054d891
amareharuno <leremeeva174@gmail.com>
2019-03-29 00:12:20 +0300

    Task 11

--- pz1/tasks/Task11.js
+++ pz1/tasks/Task11.js
@@ -3,3 +3,20 @@
     Задание: преобразовать строку в 'varTextHello'.
 */
 console.log("Task 11");
+const str = 'var_text_hello';
+console.log(`Initial string: '${str}'`);
+
+const removeUnderlines = (string) => {
+    let newStr = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== '_') {
+            newStr += string[i];
+        } else {
+            newStr += string[i+1].toUpperCase();
+            i++;
+        }
+    }
+    return newStr;
+};
+
+console.log(`Result: ${removeUnderlines(str)}`);
