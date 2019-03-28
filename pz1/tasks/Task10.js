e3fbbc913e48702a8925df409f530221446928ef
amareharuno <leremeeva174@gmail.com>
2019-03-29 00:00:17 +0300

    Task 10

--- pz1/tasks/Task10.js
+++ pz1/tasks/Task10.js
@@ -3,3 +3,15 @@
     Задание: проверить, что первым символом этой строки является буква 'a'. Результат типа boolean.
 */
 console.log("Task 10");
+
+const string = 'abcde';
+console.log(`Initial string: '${string}'`);
+
+const checkFirstLetterIsA = () => {
+    return string[0] === 'a';
+    // OR:
+    // return string.charAt(0) === 'a';
+};
+
+console.log(`First letter is 'a'?`);
+console.log(checkFirstLetterIsA());
