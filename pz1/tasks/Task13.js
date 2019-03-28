03d3dd1fcf8d4eccd8daa53f3ef03ac0a6381175
amareharuno <leremeeva174@gmail.com>
2019-03-29 00:48:53 +0300

    Task 13

--- pz1/tasks/Task13.js
+++ pz1/tasks/Task13.js
@@ -3,3 +3,26 @@
     Высоту треугольника передать параметром в функцию.
 */
 console.log("Task 13");
+
+const drawTriangle = (height) => {
+    console.log(`Height: ${height}`);
+    let bottomLineLength = 1 + (height - 1) * 2;
+
+    for (let i = 1; i <= height; i++) {
+        let numberOfTicksInLine = 1 + (i - 1) * 2;
+        let numberOfSpacesFromOneSide = (bottomLineLength - numberOfTicksInLine) / 2;
+
+        let spacesStr = "";
+        let symbolStr = "";
+        for (let j = 1; j <= numberOfSpacesFromOneSide; j++) {
+            spacesStr += ' ';
+        }
+        for (let j = 1; j <= numberOfTicksInLine; j++) {
+            symbolStr += '^';
+        }
+
+        console.log(spacesStr + symbolStr + spacesStr);
+    }
+};
+
+drawTriangle(10);
