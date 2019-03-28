c39e1ad35dc401707f27e9e285c6407fcfcd2d1d
amareharuno <leremeeva174@gmail.com>
2019-03-28 23:39:50 +0300

    Task8

--- pz1/tasks/Task8.js
+++ pz1/tasks/Task8.js
@@ -3,3 +3,17 @@
     Максимальное число должно быть не больше 18000.
 */
 console.log("Task 8");
+
+const printFibonacciSeries = (x1, x2) => {
+    if (x1 === 1 && x2 ===1) {
+        console.log(x1);
+        console.log(x2);
+    }
+    let sum = x1 + x2;
+    if (sum <= 18000) {
+        console.log(sum);
+        printFibonacciSeries(x2, sum);
+    }
+};
+
+printFibonacciSeries(1, 1);
